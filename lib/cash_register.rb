3fdd0030fcb482ed1bba577d04fc4a30a3d4731c
Maryna Voitenko <maryna.voitenko@gmail.com>
2017-11-10 02:54:33 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,7 +16,7 @@ class CashRegister
   end
 
   def apply_discount
-    self.total = self.price - self.price * self.discount / 100
+    self.total = self.total - self.total * self.discount / 100
   end
 
 end
